727d506df3e089f21af0d9d8d0a2a329aa36e6b1
Zach Carrera <zachcarrera@gmail.com>
2023-04-03 22:58:21 -0700

    add CafeUtil.java and TestCafe.java

---
+++ CafeUtil.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+public class CafeUtil {
+    public int getStreakGoal() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    public int getStreakGoal(int numWeeks) {
+        int sum = 0;
+        for (int i = 1; i <= numWeeks; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    public double getOrderTotal(double[] prices) {
+        double sum = 0;
+        for (double price : prices) {
+            sum += price;
+        }
+        return sum;
+    }
+    public void displayMenu(ArrayList<String> menuItems) {
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println(i + " " + menuItems.get(i));
+        }
+    }
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.println(String.format("Hello, %s! There are %o people in front of you", userName, customers.size()));
+        customers.add(userName);
+        System.out.println(customers);
+    }
+    public void printPriceChart(String product, double price, int maxQuantity) {
+        System.out.println(product);
+        for (int i = 1; i <= maxQuantity; i++) {
+            System.out.println(i + String.format(" - $%.2f", (price * i)));
+        }
+    }
+    public void printPriceDiscountChart(String product, double price, int maxQuantity) {
+        System.out.println(product);
+        for (int i = 1; i <= maxQuantity; i++) {
+            double discount = (i - 1) * 0.5;
+            System.out.println(i + String.format(" - $%.2f", (price * i - discount)));
+        }
+    }
+    public boolean displayMenu(ArrayList<String> menuItems, ArrayList<Double> prices) {
+        if (menuItems.size() != prices.size()) {
+            return false;
+        }
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println(i + " " + menuItems.get(i) + String.format(" -- $%.2f", prices.get(i)));
+        }
+        return true;
+    }
+}
